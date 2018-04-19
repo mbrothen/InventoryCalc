5ddbcf1e6b491be551319c93e02d784821d8dcc8
mbrothen <mbrothen@gmail.com>
2018-04-18 19:27:38 -0500

    past accept calculator working

---
+++ requirements.ts
@@ -0,0 +1,105 @@
+Final Web Project
+Assignment
+Final Web Project
+You're required to build a small Web site on your own by following the instructions given in the text.  This is an individual Web Project.  You may ask your fellow classmates to help you but you should learn to build it by yourself to get the most out of this course.
+
+Build your Web Project by following the "Individual Case Project" located at the end of each Chapter.  These are the rubrics/criteria that I will be using to grade your Final Web Project on.
+
+
+
+    The instructions below are also found on the back of the Chapter indicated.  You don't have to work in the same order as outlined below; however, I recommend that you follow the outline and keep a schedule to complete each phase timely.   You have until the last day of class to complete this project.   Thus, there is no due date for each part except Part 1 (Project Topic).  So work at your own pace at your own time.  I am only grading on your Final project that's due on the last day of class, so make sure that all phases are complete in order to earn the most points for this project.
+
+    This project is worth 1,000 points and 30% of your total grade.  It's unlikely to pass this course with a "C" or better without completing this Final Web Project.
+
+Submission
+
+When you've completed all the phases and ready to submit for grading, please submit only the URL to your project page on Apollo.  Make sure to validate all pages (CSS & HTML) and double-check to ensure that every link and script works.  
+
+Good luck and have fun!
+
+
+Points:  1,000
+
+Due Date:  Monday, April 23, 2018
+
+Item
+Chapter 1 - Pick A Topic
+The Individual Case Project for each chapter in this book will build on a website that you create on a subject of your choice.
+
+    To begin, choose a topic for your website. This can be a topic related to your major, or a personal interest or activity.
+    Plan a website containing at least four pages with a common layout and navigation system. Note that you’ll add pages to your site in later chapters, so ensure that your navigation system can support additional content.
+    Add a script element that links to the modernizr.custom.js file in each HTML document in your website. Ensure that all of your web pages pass validation.
+(See page 72)
+
+
+Note:  There's an Assignment Due for this part in Unit 1 - Apply.
+
+
+
+Item
+Chapter 2 - Mathematical Calculation
+Plan and add a feature to one of the web pages in your personal site that uses at least one function to perform a mathematical calculation based on user input. Test the page to ensure it works as planned.
+
+(See page 146)
+
+Item
+Chapter 3 - Decisions
+Plan and add a feature to one of the web pages in your personal site that incorporates content or functionality created by a series of if, if/else, and/or else if statements, or by a switch statement. If your page requires an event listener, create one that’s backward- compatible with older versions of Internet Explorer. View and test your page in one or more browsers as appropriate to ensure it works as you expect.
+
+(See Page 311)
+
+    Item
+    Chapter 4 - Exception Handling
+    Add exception handling to the code for one of the forms on your personal web site. If your site does not include a form, add one first. Your code should display one or more relevant error messages in an appropriate location. After you finalize your code, write a summary of the debugging methods from this chapter that you used in this project, describing how you used each one in your code.
+
+    (See page 288)
+
+    Item
+    Chapter 5 - Web Security
+    Add a page to your individual website that educates visitors about web security. Report the values of at least six properties from Tables 5-10 (pp.344-345) and 5-11 (p. 346)to illustrate the breadth of information about a user’s computer that a web app can access. Perform a web search on practices for using the web safely, and include links to at least three sources, along with a one-sentence summary of each.
+
+    (See page 362)
+
+    Item
+    Chapter 6 - Form Validations
+    Add validation the code for one of the forms on your individual website. First, ensure that your form uses at least three of the following field types: check boxes, text boxes, option buttons, selection lists, and text areas. Then, program validation for your form ensuring that users enter values or make selections in all fields, and verifying at least one other aspect of at least one of the fields. Provide appropriate feedback to users when the form fails validation. Test your completed program until all validation works reliably with different combinations of valid and erroneous data.
+
+    (See page 450)
+
+    Item
+    Chapter 7 - Dates and Times
+    Expand your individual website to include a page that calculates the time elapsed since a date entered by a user. The page should include a form that allows users to enter a day, month, and year. The page should then calculate and display the elapsed time in years, months, and days. Note that your program must include code to convert day values in excess of into months, and months in excess of into years.
+
+    (See page 531)
+
+    Item
+    Chapter 8 - Feedback Form
+    Enhance the feedback form in your project to enable users to choose one or more options from a list of at least five options. Include code that adds user selections to either an array or an object, and ensure that if a user deselects one of the options, it is removed from the array or object. Add code to convert the array or object to a string.
+
+    (See page 617)
+
+    Item
+    Chapter 9 - Security Issues
+    Enhance the personal website you’ve created in the preceding chapters of this book to prevent security issues. To do this, review each form field in your site and identify any additional validation that you could reasonably add. In particular, look for situations where you could use a regular expression to limit the allowable characters in order to exclude characters used in creating JavaScript code. Use word-processing software to create a table like Table 9-4, which includes an example. Add a row for each form field in your website.
+
+
+
+        When you’ve reviewed each field in your website, add the validation described in your table. Test each field until you’re satisfied with your new validation code.
+
+    (See page 679)
+
+    Item
+    Chapter 10 - SKIP THIS CHAPTER
+    *** We will not do Chapter 10 **
+
+    Item
+    Chapter 11 - AJAX
+    Identify data provided by an Ajax service that you’d like to include in your personal website. You should choose a web service other than those used in the chapter and the Hands-on Projects. If you have an idea for data you’d like to access but are unsure what service might provide that data, perform a web search on a description of the data plus “API.” For instance, if you were looking for a source of tide tables, you might search for “tide tables API.” Use the documentation for the web service to construct an Ajax request and to display selected data from the service on your website. Note that if you don’t have experience with writing PHP, you may need to examine a number of potential APIs to identify one that allows JSON-P or CORS requests, which don’t require you to run a proxy.
+
+    (See page 811)
+
+    Item
+    Chapter 12 - jQuery
+    In your individual website, revise a function to use jQuery selectors and methods. Identify a function that contains at least three selectors that you can replace with jQuery selectors, and that performs at least one DOM traversal or CSS change that you can replace with a jQuery method. Comment out the code you replace rather than deleting it. Be sure to link to the jQuery library in all HTML documents that link to the .js file you’ve updated. When your revisions are done, test all pages that use the function to ensure they still perform as they did when the function was written in plain JavaScript.
+
+    (See page 839)
